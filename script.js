28cd0d74b0fe385f36829e2406131b56df78b266
yousseff3474 <youssef34743791@gamil.com>
2025-08-10 06:19:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const a = "10"
+const b = "15"
+console.log(a- b);
+console.log(parseInt(a)+ parseInt (b));
+console.log(a/b);
+console.log(a * b);
+
+const p = true
+const o = 1
+console.log(o === p);
+
+
+const langs = [1 ,2, 3, 4, "yossef", "sayed"]
+langs.push("mohamed")
+langs [6] = "mostat"
+langs.pop()
+console.log(langs); 
